7df0136db62e7454350052be7c77198a9f95bc22
sreid8 <sean.reid8@gmail.com>
2016-10-31 21:47:45 -0400

    finished an untested copy of the EventListenerRegistry. starting the implementation of a Factory/Manager thing to create instance sof EventListenerRegistries on a key String system. Committing now since I'll have to refactor the EventListenRegistry away from the singleton thing it's doing now

---
+++ src/com/sreid/basiceventlib/registry/EventListenerRegistryManager.java
@@ -0,0 +1,9 @@
+package com.sreid.basiceventlib.registry;
+
+/**
+ * creates new instances of {@link com.sreid.basiceventlib.registry.EventListenerRegistry} as needed
+ * and allows querying to use the same one based on a String key
+ * Created by sean on 10/31/16.
+ */
+public class EventListenerRegistryManager {
+}
